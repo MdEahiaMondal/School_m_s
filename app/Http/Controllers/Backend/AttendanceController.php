40ba8fee54ad692771372c203ea0440c3401a860
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-11-20 14:15:56 +0600

    attendance create done

--- app/Http/Controllers/Backend/AttendanceController.php
+++ app/Http/Controllers/Backend/AttendanceController.php
@@ -7,6 +7,7 @@ use App\Attendance;
 use App\Student;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AttendanceController extends Controller
 {
@@ -21,18 +22,22 @@ class AttendanceController extends Controller
 
     public function create()
     {
-        //
+        $students = Student::latest()->get();
+        $classes = AllClass::latest()->get();
+        return view('backend.pages.attendance.create', compact('students', 'classes'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function store(Request $request)
     {
-        //
+         $request->validate($this->AttendanceStoreValidator(), $this->CustomErrorMessage());
+
+         $request['teacher_id'] = Auth::user();
+
+         Attendance::create($request->all());
+
+         return redirect()->route('attendances.index')->with('success', 'Attendance Create Successfully !');
     }
 
     /**
@@ -79,4 +84,24 @@ class AttendanceController extends Controller
     {
         //
     }
+
+    private function AttendanceStoreValidator()
+    {
+        return [
+            'student_id' => 'required',
+            'class_id' => 'required',
+            'attendance_status' => 'required',
+            'attendance_date' => 'required',
+        ];
+    }
+
+
+    public function CustomErrorMessage()
+    {
+        return [
+            'student_id.required' => 'Student field Required',
+            'class_id.required' => 'Class field Required',
+        ];
+    }
+
 }
--- resources/views/backend/pages/attendance/create.blade.php
+++ resources/views/backend/pages/attendance/create.blade.php
@@ -23,78 +23,19 @@
                         </header>
                         <div class="panel-body">
                             <div class="position-center">
-                                <form class="form-horizontal" role="form" method="post" action="{{ route('students.store') }}">
+                                <form class="form-horizontal" role="form" method="post" action="{{ route('attendances.store') }}">
                                     @csrf
                                     <div class="form-group">
-                                        <label for="name" class="col-lg-2 col-sm-2 control-label">Name <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
+                                        <label for="inputSuccess" class="col-lg-2 col-sm-2 control-label">Student<sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
                                         <div class="col-lg-10">
-                                            <input type="text" value="{{ old('name') }}" name="name" class="form-control" id="name" placeholder="Name">
-                                            @error('name')
-                                                <span class="text-danger" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="inputEmail1" class="col-lg-2 col-sm-2 control-label">Email <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
-                                        <div class="col-lg-10">
-                                            <input type="email" value="{{ old('email') }}"  name="email" class="form-control" id="inputEmail1" placeholder="Email">
-                                            @error('email')
-                                                <span class="text-danger" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="inputPassword1" class="col-lg-2 col-sm-2 control-label">Password <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
-                                        <div class="col-lg-10">
-                                            <input type="password" class="form-control" name="password" id="inputPassword1" placeholder="Password">
-                                            @error('password')
-                                                <span class="text-danger" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="phone" class="col-lg-2 col-sm-2 control-label">Phone <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
-                                        <div class="col-lg-10">
-                                            <input type="text" value="{{ old('phone') }}" name="phone" class="form-control" id="phone" placeholder="Phone">
-                                            @error('phone')
-                                            <span class="text-danger" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="inputSuccess" class="col-lg-2 col-sm-2 control-label">Parent <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
-                                        <div class="col-lg-10">
-                                            <select name="parent_id" class="form-control m-bot15">
-                                                <option value="">===>Choose Parents===></option>
-                                                @foreach($parents as $parent)
-                                                    <option value="{{ $parent->id }}">{{ $parent->user->name }}</option>
+                                            <select name="student_id" class="form-control m-bot15">
+                                                <option value="">===>Choose Student===></option>
+                                                @foreach($students as $student)
+                                                    <option value="{{ $student->id }}">{{ $student->user->name }}</option>
                                                 @endforeach
                                             </select>
-                                            @error('parent_id')
-                                            <span class="text-danger" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
 
-                                    <div class="form-group">
-                                        <label for="parent_phone" class="col-lg-2 col-sm-2 control-label">Parent Phone <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
-                                        <div class="col-lg-10">
-                                            <input type="text" value="{{ old('parent_phone') }}" name="parent_phone" class="form-control" id="parent_phone" placeholder="Parent Phone">
-                                            @error('parent_phone')
+                                            @error('student_id')
                                             <span class="text-danger" role="alert">
                                                     <strong>{{ $message }}</strong>
                                                 </span>
@@ -121,10 +62,13 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="roll_number" class="col-lg-2 col-sm-2 control-label">Roll No <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
+                                        <label for="inputSuccess" class="col-lg-2 col-sm-2 control-label">Class <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
                                         <div class="col-lg-10">
-                                            <input type="text" value="{{ old('roll_number') }}" name="roll_number" class="form-control" id="roll_number" placeholder="Class Roll Number">
-                                            @error('roll_number')
+                                            <select name="attendance_status" class="form-control m-bot15">
+                                                <option value="1">Present</option>
+                                                <option value="0">Absent</option>
+                                            </select>
+                                            @error('attendance_status')
                                             <span class="text-danger" role="alert">
                                                     <strong>{{ $message }}</strong>
                                                 </span>
@@ -134,50 +78,10 @@
 
 
                                     <div class="form-group">
-                                        <label for="age" class="col-lg-2 col-sm-2 control-label">Age <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup></label>
+                                        <label for="attendance_date" class="col-lg-2 col-sm-2 control-label">Attendance Date </label>
                                         <div class="col-lg-10">
-                                            <input type="text" value="{{ old('age') }}" name="age" class="form-control" id="age" placeholder="Age">
-                                            @error('age')
-                                            <span class="text-danger" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="age" class="col-lg-2 col-sm-2 control-label">Gender <sup class="text-danger" style="font-size: 9px"> <i class="fa fa-asterisk"></i> </sup> </label>
-
-                                        <div class="col-sm-10 icheck ">
-                                            <div class="radio square-green ">
-                                                <input tabindex="3" type="radio" checked value="male" name="gender">
-                                                <label>Male</label>
-                                            </div>
-
-                                            <div class="radio square-green ">
-                                                <input tabindex="3" type="radio" value="female"  name="gender">
-                                                <label>Female </label>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="date_of_birth" class="col-lg-2 col-sm-2 control-label">Birth day </label>
-                                        <div class="col-lg-10">
-                                            <input type="date" value="{{ old('date_of_birth') }}" name="date_of_birth" class="form-control" id="date_of_birth" placeholder="Birth day">
-                                            @error('date_of_birth')
-                                            <span class="text-danger" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="address" class="col-lg-2 col-sm-2 control-label">Address </label>
-                                        <div class="col-lg-10">
-                                            <textarea name="address" id="address" cols="30" class="form-control" rows="5"> {{ old('address') }}  </textarea>
-                                            @error('address')
+                                            <input type="date" value="<?php echo date('Y-m-d'); ?>" name="attendance_date" class="form-control" id="attendance_date">
+                                            @error('attendance_date')
                                             <span class="text-danger" role="alert">
                                                     <strong>{{ $message }}</strong>
                                                 </span>
@@ -189,7 +93,7 @@
                                         <div class="col-lg-offset-2 col-lg-10">
                                             <button type="submit" class="btn btn-primary">Create</button>
                                             <button type="reset" class="btn btn-danger">Reset</button>
-                                            <a href="{{ route('students.index') }}" class="btn btn-default">Back</a>
+                                            <a href="{{ route('attendances.index') }}" class="btn btn-default">Back</a>
                                         </div>
                                     </div>
                                 </form>
@@ -212,5 +116,6 @@
     <script src="{{ asset('backend/js/iCheck/jquery.icheck.js') }}"></script>
 
     <script src="{{ asset('backend/js/icheck-init.js') }}"></script>
+
 
 @endpush
